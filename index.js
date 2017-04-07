df192d5325598708fe0148c093f58ec403b47290
Rahman, Mohammed (DS) <Mohammed.Rahman@cdk.com>
2017-04-07 15:27:01 +0530

    Added emit function

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ var pubSub = (function (eventName) {
         events[eventName].push(callback);
     };
 
+    var emit = function (data) {
+
+        if (!events[eventName]) return;
+
+        var subscribers = events[eventName];
+
+        for (var i = 0; i < subscribers.length; i++) {
+            subscribers[i](data);
+        }
+    };
+
     return {
         subscribe: subscribe,
         emit: emit
