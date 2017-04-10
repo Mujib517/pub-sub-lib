931e6ad8cfdd6c9ee6d399daf1d697bd960533e6
Rahman, Mohammed (DS) <Mohammed.Rahman@cdk.com>
2017-04-10 11:14:43 +0530

    multicasting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function PubSub() {
-    
+
     //if user doesn't create object using new operator
-    if (PubSub.prototype.constructor.name!=='PubSub') return new PubSub();
-      this.__events = {};
+    if (PubSub.prototype.constructor.name !== 'PubSub') return new PubSub();
+    this.__events = {};
 }
 
 PubSub.prototype.subscribe = function (event, callback) {
@@ -24,13 +24,26 @@ PubSub.prototype.emit = function (event, data) {
     }
 };
 
+//notify first n subscribers
+PubSub.prototype.multicast = function (event, data, n) {
+
+    var subscribers = this.__events[event];
+
+    if (subscribers) {
+        var length = subscribers.length < n ? subscribers.length : n;
+
+        for (var i = 0; i < length; i++) {
+            subscribers[i](data);
+        }
+    }
+};
 
 /*test*/
 
-var pubSub = PubSub();
+// var pubSub = new PubSub();
 
-pubSub.subscribe('evt', function (data) { console.log('hell') });
-pubSub.subscribe('evt', function (data) { console.log('hell2') });
-pubSub.subscribe('evt', function (data) { console.log('hell3') });
+// pubSub.subscribe('evt', function (data) { console.log('hell') });
+// pubSub.subscribe('evt', function (data) { console.log('hell2') });
+// pubSub.subscribe('evt', function (data) { console.log('hell3') });
 
-pubSub.emit('evt', { data: 'data' });
+// pubSub.multicast('evt', { data: 'data' },1);
